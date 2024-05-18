2fd017d89adf63a672f1f0a7f9a32a4095d2262c
Jonas Riedel <jonasriedel@pm.me>
2024-05-18 11:59:39 +0200

    Rearrange skills

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -15,18 +15,18 @@
   
 <script setup lang="ts">
 const skills = [
-    { name: 'JavaScript/TypeScript', description: 'Front-end scripting language' },
-    { name: 'Vue.js', description: 'JavaScript framework for building user interfaces' },
-    { name: 'Node.js', description: 'JavaScript runtime for server-side development' },
     { name: 'Golang', description: 'Programming language for building scalable and reliable software' },
     { name: 'Kubernetes', description: 'Container orchestration platform' },
     { name: 'ArgoCD', description: 'Continuous Delivery tool for Kubernetes' },
     { name: 'Docker', description: 'Containerization platform' },
     { name: 'Rust', description: 'Systems programming language' },
     { name: 'Linux', description: 'Operating system kernel' },
+    { name: 'Git', description: 'Version control system for tracking changes in source code' },
     { name: 'Java', description: 'Versatile and widely used programming language' },
     { name: 'Databases (SQL)', description: 'Database management using SQL' },
-    { name: 'Git', description: 'Version control system for tracking changes in source code' },
+    { name: 'JavaScript/TypeScript', description: 'Front-end scripting language' },
+    { name: 'Vue.js', description: 'JavaScript framework for building user interfaces' },
+    { name: 'Node.js', description: 'JavaScript runtime for server-side development' },
 ];
 </script>
   
